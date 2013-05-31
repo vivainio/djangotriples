5a70e2dda199f9ba2e3123a0516c80f3e15fed27
Ville M. Vainio <vivainio@gmail.com>
2013-05-31 13:46:38 +0300

    works

--- trapi/models.py
+++ trapi/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from django.contrib import admin
 # Create your models here.
 
 
@@ -11,10 +12,16 @@ class Predicate(models.Model):
 
 class AnnotationTx(models.Model):
 	tstamp = models.DateTimeField(auto_now_add = True)
-	
+
 
 class Triple(models.Model):
 	tx = models.ForeignKey(AnnotationTx)
 	s = models.ForeignKey(Subject)
 	p = models.ForeignKey(Predicate)
 	o = models.TextField()
+
+admin.site.register(Subject)
+admin.site.register(Predicate)
+admin.site.register(AnnotationTx)
+admin.site.register(Triple)
+
